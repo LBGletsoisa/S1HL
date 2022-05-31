25a684585b4d54b7c00a3c0cc6c4173ce252914f
LBGletsoisa <105360000+LBGletsoisa@users.noreply.github.com>
2022-05-31 02:06:09 +0200

    Add files via upload

---
+++ S1HL.py
@@ -0,0 +1,10 @@
+import urllib
+import re
+from bs4 import BeautifulSoup
+import streamlit as st
+
+st.set_page_config(page_title="My Website", page_icon=":tada", layout="wide")
+st.subheader("Sensor 1")
+
+st.write("Sensor 1 in the basememt tunnel, close to Grizzly section has detetected humidity levels lower than the threshold of 0%")
+
